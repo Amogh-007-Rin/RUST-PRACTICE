7ec3dea16e08b4fb2c0dadb5e65ab23b24eeeca9
Amogh-007-Rin <amoghdath233@gmail.com>
2026-08-02 23:45:00 +0100

    module-module-099

--- modules/module-099-advanced-topics-and-staying-current/exercises/tests/module_099.rs
+++ modules/module-099-advanced-topics-and-staying-current/exercises/tests/module_099.rs
@@ -1,6 +1,34 @@
-use module_099_exercises::placeholder;
+use module_099_exercises::*;
 
 #[test]
-fn placeholder_test() {
-    assert_eq!(placeholder(), 42);
+fn test_fixed_size_array_sum() {
+    assert_eq!(fixed_size_array_sum([1, 2, 3, 4, 5]), 15);
+    assert_eq!(
+        fixed_size_array_sum::<0>([]),
+        0,
+        "zero-length array sums to 0"
+    );
+    assert_eq!(fixed_size_array_sum([42]), 42);
+    assert_eq!(fixed_size_array_sum([10, 20, 30]), 60);
+}
+
+#[test]
+fn test_gat_container_for_vec() {
+    let v: Vec<i32> = vec![10, 20, 30];
+    assert_eq!(Container::get(&v, 0), Some(&10));
+    assert_eq!(Container::get(&v, 1), Some(&20));
+    assert_eq!(Container::get(&v, 2), Some(&30));
+    assert_eq!(Container::get(&v, 3), None);
+    assert_eq!(Container::get(&v, 100), None);
+}
+
+#[test]
+fn test_const_evaluation() {
+    assert_eq!(demonstrate_const_evaluation(), 120);
+}
+
+#[test]
+fn test_static_assertion() {
+    let msg = demonstrate_static_assertion();
+    assert_eq!(msg, "static assertion passed");
 }
