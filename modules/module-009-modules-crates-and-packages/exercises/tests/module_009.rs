7bc813841ac9e70c1e0b238f1da7294f4cb87e80
Amogh-007-Rin <amoghdath233@gmail.com>
2026-08-02 23:44:49 +0100

    module-module-009

--- modules/module-009-modules-crates-and-packages/exercises/tests/module_009.rs
+++ modules/module-009-modules-crates-and-packages/exercises/tests/module_009.rs
@@ -1,6 +1,38 @@
-use module_009_exercises::placeholder;
+use module_009_exercises::{math, shout_sum, utils};
 
 #[test]
-fn placeholder_test() {
-    assert_eq!(placeholder(), 42);
+fn math_add() {
+    assert_eq!(math::add(2, 3), 5);
+}
+
+#[test]
+fn math_sub() {
+    assert_eq!(math::sub(5, 2), 3);
+}
+
+#[test]
+fn math_mul() {
+    assert_eq!(math::mul(4, 3), 12);
+}
+
+#[test]
+fn utils_shout() {
+    assert_eq!(utils::shout("hi"), "HI!");
+}
+
+#[test]
+fn utils_is_blank() {
+    assert!(utils::is_blank(""));
+    assert!(utils::is_blank("   "));
+    assert!(!utils::is_blank("  x"));
+}
+
+#[test]
+fn shout_sum_combines_modules() {
+    assert_eq!(shout_sum(2, 3), "5!");
+}
+
+#[test]
+fn shout_sum_negative() {
+    assert_eq!(shout_sum(-2, -3), "-5!");
 }
